ffae0886ab0d35fa78b12f578e0d913b42fa83ea
Mauro1442 <maurodirosa19@gmail.com>
2022-12-10 16:40:30 +0100

    crud

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+
+
+
+Route::resource('/products', ProductController::class);
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+//Route::get('/products', [ProductController::class, 'index']);
+//Route::post('/products', [ProductController::class, 'store']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
