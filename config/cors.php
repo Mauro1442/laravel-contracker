482d14687d5cc20bc3301c9bc0a719268ec84fc6
Mauro Di Rsa <maurodirosa19@gmail.com>
2022-12-20 12:01:25 +0100

    cors

--- config/cors.php
+++ config/cors.php
@@ -21,14 +21,14 @@ return [
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['*'],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => false,
 
-    'max_age' => 0,
+    'max_age' => false,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
