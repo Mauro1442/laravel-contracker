f62717ff6365a1ec235fbddb8f0eaa190570a1be
Mauro Di Rsa <maurodirosa19@gmail.com>
2023-01-30 11:57:28 +0100

    change prc

--- app/Http/Middleware/SetApiLang.php
+++ app/Http/Middleware/SetApiLang.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 
 class SetApiLang {
 
+
+  
   public function handle(Request $request, Closure $next) {
     $lang = $request->server('HTTP_ACCEPT_LANGUAGE');
     if (str_starts_with($lang, 'de')) {
